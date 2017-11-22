98c6e081edd0f961d87040d3165f9f1bd807c65a
David <dm-17@users.noreply.github.com>
2017-11-22 10:26:09 +0100

    Create date2epoch.sh

---
+++ date2epoch.sh
@@ -0,0 +1,18 @@
+# Convert the human-readable timestamp in epoch format
+
+#echo "The input must match the following pattern:"
+#echo "Syntax is: $0 MM/DD/YYYY HH:mm:ss"
+
+# date +%s -d"11/17/2017 08:09:00"
+
+if [ -n $1 ] && [[ $1 =~ ^[01]?[0-9]/[01]?[0-9]/[12][0-9][0-9][0-9] ]]; then
+        if [ -n $2 ] && [[ $2 =~ [0-2]{0,1}[0-9]:[0-5]{0,1}[0-9]:[0-5]{0,1}[0-9]$ ]] ; then
+                date +%s -d "$1 $2"
+                exit
+        else echo "*** Error ***: Invalid time format"
+        fi
+else echo "*** Error ***: Invalid date format"
+fi
+
+echo "Syntax is: $0 \"MM/DD/YYYY HH:mm:ss\""
+exit
